98e79478ebb02189ce77c07cb7954c28e5a9bc75
nicolo fossati <nicofossa01@gmail.com>
2023-03-08 13:39:50 +0100

    MILESTONE 0

--- js/main.js
+++ js/main.js
@@ -21,3 +21,41 @@ Angela Lopez	    Social Media Manager	angela-lopez-social-media-manager.jpg
 Scott Estrada	    Developer	            scott-estrada-developer.jpg
 Barbara Ramos	    Graphic Designer	    barbara-ramos-graphic-designer.jpg
 */
+
+const team = [
+    {
+        name : 'Wayne Barnett',
+        position : 'Founder & CEO',
+        photo : 'wayne-barnett-founder-ceo.jpg'
+    },
+
+    {
+        name : 'Angela Caroll',
+        position : 'Chief Editor',
+        photo : 'angela-caroll-chief-editor.jpg'
+    },
+
+    {
+        name : 'Walter Gordon',
+        position : 'Office Manager',
+        photo : 'walter-gordon-office-manager.jpg'
+    },
+
+    {
+        name : 'Angela Lopez',
+        position : 'Social Media Manager',
+        photo : 'angela-lopez-social-media-manager.jpg'
+    },
+
+    {
+        name : 'Scott Estrada',
+        position : 'Developer',
+        photo : 'scott-estrada-developer.jpg'
+    },
+
+    {
+        name : 'Barbara Ramos',
+        position : 'Graphic Designer',
+        photo : 'barbara-ramos-graphic-designer.jpg'
+    }
+];
